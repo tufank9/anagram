73b71d33005ecdd0ca00fa88a950786c44375441
tufan <tufankorkmazel3@gmail.com>
2023-12-18 00:42:33 +0300

    if ile anagram kontrol sorgusu yapıldı.

--- anagram.js
+++ anagram.js
@@ -1,2 +1,9 @@
 let kelime1 = ["apolitik", "orman", "kış", "kol", "sandalye", "kürek"];
 let kelime2 = ["politika", "roman", "pış", "defter", "masa", "ürkek"];
+
+for (let i = 0; i < kelime1.length; i++){
+    if (kelime1[i].length !== kelime2[i].length){
+        console.log(`${kelime1[i]} ve ${kelime2[i]} uzunlukları eşit değil, anagram kontrolü yapılamaz.`);
+        continue;
+    }
+}
